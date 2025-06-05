1ff4eb66ddf1c131b236ac3c7af8294d1e8eab7a
Raksha Ashtankar <rakshaashtankar@Rakshas-MacBook-Air.local>
2025-06-05 17:30:58 +0530

    Added UniqueConstraint in Novels class for all fields(except id)  and also added length property to all columns excxept id as creation og unique key supports max key length as 3072 bytes.

--- src/main/java/com/springboot/nmsbackend/model/Novels.java
+++ src/main/java/com/springboot/nmsbackend/model/Novels.java
@@ -5,23 +5,23 @@ import lombok.Data;
 
 @Entity
 @Data
-@Table
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"novel_author", "novel_genre", "novel_synopsis", "novel_title"})})
 public class Novels {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(nullable = false)
+    @Column(name = "novel_title", nullable = false, length = 100)
     private String novelTitle;
 
-    @Column(nullable = false)
+    @Column(name = "novel_author", nullable = false, length = 100)
     private String novelAuthor;
 
-    @Column(nullable = false)
+    @Column(name = "novel_genre", nullable = false, length = 50)
     private String novelGenre;
 
-    @Column(nullable = false)
+    @Column(name = "novel_synopsis", nullable = false, length = 200)
     private String novelSynopsis;
 
 }
