7c92ace87487e9899982282c95d6e62e20b95ef9
Raksha Ashtankar <rakshaashtankar@Rakshas-MacBook-Air.local>
2025-06-05 13:21:52 +0530

    Initial commit

---
+++ src/main/java/com/springboot/nmsbackend/NmsbackendApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.nmsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmsbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NmsbackendApplication.class, args);
+	}
+
+}
