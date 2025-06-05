99ea358a3c7e97153e9830ac0688ab636ff8f359
Raksha Ashtankar <rakshaashtankar@Rakshas-MacBook-Air.local>
2025-06-05 13:42:41 +0530

    Created a NovelRepository interface which extends JPARepository.

---
+++ src/main/java/com/springboot/nmsbackend/repository/NovelsRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.nmsbackend.repository;
+
+import com.springboot.nmsbackend.model.Novels;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NovelsRepository extends JpaRepository<Novels, Integer> {
+}
